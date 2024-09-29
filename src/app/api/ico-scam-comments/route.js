e888b144e1268cec53ae84ea46f588ba69bed137
Majid Hasan <majidhasan@Majids-MacBook-Pro.local>
2024-09-29 21:53:37 +0600

    Ico Scam Comment Routes Done

--- src/app/api/ico-scam-comments/route.js
+++ src/app/api/ico-scam-comments/route.js
@@ -1,4 +1,4 @@
-import { prisma } from "@/lib/prisma";
+import prisma from "@/lib/prisma";
 
 // Helper function for pagination and filtering
 const paginateAndFilter = (icoScamId, page, limit) => {
