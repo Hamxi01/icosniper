b2a2a3219dcf89c3ba58809a010b62849862acf6
Majid Hasan <majidhasan@Majids-MacBook-Pro.local>
2024-09-29 22:12:47 +0600

    Changed on the new single page on admin part

--- src/app/admin/news/single/page.jsx
+++ src/app/admin/news/single/page.jsx
@@ -1,10 +1,10 @@
 "use client";
 import { useSearchParams, useRouter } from "next/navigation";
 import axios from "axios";
-import { useEffect, useState, useRef, Suspense } from "react"; // Import Suspense from React
-import dynamic from "next/dynamic"; // Import dynamic from Next.js
+import { useEffect, useState, useRef } from "react";
+import dynamic from "next/dynamic";
 import "react-quill/dist/quill.snow.css";
-import { handleFileUpload } from "@/lib/firebaseFileManage"; // Assume you have this utility function for handling Firebase uploads
+import { handleFileUpload } from "@/lib/firebaseFileManage";
 
 const ReactQuill = dynamic(() => import("react-quill"), { ssr: false });
 
@@ -65,8 +65,8 @@ const AddEditBlogPage = () => {
     setUploading(true);
     try {
       const mediaUrl = await handleFileUpload(file, "news/");
-      setThumbnail(mediaUrl); // Update thumbnail state with the uploaded image URL
-      return mediaUrl; // Return the uploaded URL for later use
+      setThumbnail(mediaUrl);
+      return mediaUrl;
     } catch (error) {
       console.error("Error uploading image:", error);
       alert("Error uploading image. Please try again.");
@@ -122,179 +122,173 @@ const AddEditBlogPage = () => {
       input.onchange = async () => {
         const file = input.files[0];
         if (file) {
-          const imageUrl = await handleImageUpload(file); // Get the uploaded image URL
-          const quill = quillRef.current.getEditor(); // Get the Quill editor instance
-          const range = quill.getSelection(); // Get the current selection range
-          quill.insertEmbed(range.index, "image", imageUrl); // Insert image URL into editor
+          const imageUrl = await handleImageUpload(file);
+          const quill = quillRef.current.getEditor();
+          const range = quill.getSelection();
+          quill.insertEmbed(range.index, "image", imageUrl);
         }
       };
     }
   };
 
   return (
-    <Suspense fallback={<div>Loading...</div>}>
-      {" "}
-      {/* Wrap your component with Suspense */}
-      <div className="flex flex-row min-h-screen dark:bg-gray-900 text-white">
-        <div className="w-3/4 p-6">
-          <h1 className="text-2xl font-bold mb-6">
-            {blogId ? "Edit Blog" : "Add New Blog"}
-          </h1>
-          <form
-            onSubmit={(e) => {
-              e.preventDefault();
-              handleThumbnailUpload();
-            }}
-          >
-            <div className="mb-4">
-              <label className="block text-white mb-2">Title</label>
-              <input
-                type="text"
-                value={title}
-                onChange={(e) => setTitle(e.target.value)}
-                className="w-full px-4 py-2 rounded bg-gray-700 text-white"
-                required
-              />
-            </div>
-            <div className="mb-4">
-              <label className="block text-white mb-2">Description</label>
-              <ReactQuill
-                ref={quillRef}
-                value={description}
-                onChange={setDescription}
-                className="bg-gray-800 text-white rounded-lg shadow-lg"
-                style={{ height: "300px", marginBottom: "20px" }}
-                modules={{
-                  toolbar: [
-                    [{ header: [1, 2, false] }],
-                    ["bold", "italic", "underline", "strike"],
-                    ["link", "image", { color: [] }, { background: [] }],
-                    [{ list: "ordered" }, { list: "bullet" }],
-                    ["clean"],
-                    ["image"],
-                  ],
-                }}
-                formats={[
-                  "header",
-                  "bold",
-                  "italic",
-                  "underline",
-                  "strike",
-                  "link",
-                  "image",
-                  "list",
-                  "bullet",
-                  "color",
-                  "background",
-                ]}
-                placeholder="Write your content here..."
-              />
-              <style jsx>{`
-                .ql-toolbar {
-                  border: none;
-                  border-radius: 8px 8px 0 0;
-                  padding: 10px;
-                }
-                .ql-container {
-                  border-radius: 0 0 8px 8px;
-                  border: 1px solid #374151;
-                }
-                .ql-editor {
-                  min-height: 300px;
-                  padding: 10px;
-                }
-              `}</style>
-            </div>
-            <div className="mb-4 mt-14">
-              <label className="block text-white mb-2">Category</label>
-              <select
-                value={category}
-                onChange={(e) => setCategory(e.target.value)}
-                className="w-full px-4 py-2 rounded bg-gray-700 text-white"
-                required
-              >
-                <option value="tech">Tech</option>
-                <option value="health">Health</option>
-                <option value="business">Business</option>
-              </select>
-            </div>
-            <div className="mb-4">
-              <label className="block text-white mb-2">Tags</label>
-              <div className="flex flex-wrap">
-                {tags.map((tag, index) => (
-                  <div
-                    key={index}
-                    className="flex items-center mr-2 mb-2 bg-blue-600 rounded px-2 py-1 text-sm"
-                  >
-                    {tag}
-                    <button
-                      type="button"
-                      className="ml-2 text-white hover:text-red-400"
-                      onClick={() => handleRemoveTag(index)}
-                    >
-                      &times;
-                    </button>
-                  </div>
-                ))}
-                <input
-                  type="text"
-                  value={newTag}
-                  onChange={(e) => setNewTag(e.target.value)}
-                  onKeyDown={handleTagKeyDown}
-                  placeholder="Press Enter to add a tag"
-                  className="w-full px-4 py-2 rounded bg-gray-700 text-white"
-                />
-              </div>
-            </div>
-          </form>
-        </div>
-        <div className="w-1/4 p-6 bg-gray-800">
-          <h2 className="text-xl font-bold mb-4">Options</h2>
+    <div className="flex flex-row min-h-screen dark:bg-gray-900 text-white">
+      <div className="w-3/4 p-6">
+        <h1 className="text-2xl font-bold mb-6">
+          {blogId ? "Edit Blog" : "Add New Blog"}
+        </h1>
+        <form
+          onSubmit={(e) => {
+            e.preventDefault();
+            handleThumbnailUpload();
+          }}
+        >
           <div className="mb-4">
-            <label className="block text-white mb-2">Thumbnail</label>
-            {blogId && (
-              <img
-                src={thumbnail}
-                alt="Thumbnail preview"
-                className="w-full h-40 object-cover rounded mb-2"
-              />
-            )}
+            <label className="block text-white mb-2">Title</label>
             <input
-              className="mb-4"
-              type="file"
-              accept="image/*"
-              onChange={handleFileChange}
+              type="text"
+              value={title}
+              onChange={(e) => setTitle(e.target.value)}
+              className="w-full px-4 py-2 rounded bg-gray-700 text-white"
+              required
             />
           </div>
-          <h2 className="text-xl font-bold mb-4">Status</h2>
-          <button
-            onClick={() => toggleStatus("draft")}
-            className={`mb-2 w-full py-2 rounded ${
-              status === "draft" ? "bg-blue-600" : "bg-gray-600"
-            }`}
-          >
-            Draft
-          </button>
-          <button
-            onClick={() => toggleStatus("published")}
-            className={`w-full py-2 rounded ${
-              status === "published" ? "bg-blue-600" : "bg-gray-600"
-            }`}
-          >
-            Publish
-          </button>
-          <div className="mt-6">
-            <button
-              onClick={handleThumbnailUpload}
-              disabled={uploading}
-              className="w-full py-2 rounded bg-green-600 text-white"
-            >
-              {uploading ? "Uploading..." : blogId ? "Update Blog" : "Add Blog"}
-            </button>
+          <div className="mb-4">
+            <label className="block text-white mb-2">Description</label>
+            <ReactQuill
+              ref={quillRef}
+              value={description}
+              onChange={setDescription}
+              className="bg-gray-800 text-white rounded-lg shadow-lg"
+              style={{ height: "300px", marginBottom: "20px" }}
+              modules={{
+                toolbar: [
+                  [{ header: [1, 2, false] }],
+                  ["bold", "italic", "underline", "strike"],
+                  ["link", "image", { color: [] }, { background: [] }],
+                  [{ list: "ordered" }, { list: "bullet" }],
+                  ["clean"],
+                  ["image"],
+                ],
+              }}
+              formats={[
+                "header",
+                "bold",
+                "italic",
+                "underline",
+                "strike",
+                "link",
+                "image",
+                "list",
+                "bullet",
+                "color",
+                "background",
+              ]}
+              placeholder="Write your content here..."
+            />
+            <style jsx>{`
+              .ql-toolbar {
+                border: none;
+                border-radius: 8px 8px 0 0;
+                padding: 10px;
+              }
+              .ql-container {
+                border-radius: 0 0 8px 8px;
+                border: 1px solid #374151;
+              }
+              .ql-editor {
+                min-height: 300px;
+                padding: 10px;
+              }
+            `}</style>
           </div>
-        </div>
+          <div className="mb-4 mt-14">
+            <label className="block text-white mb-2">Category</label>
+            <select
+              value={category}
+              onChange={(e) => setCategory(e.target.value)}
+              className="w-full px-4 py-2 rounded bg-gray-700 text-white"
+              required
+            >
+              <option value="tech">Tech</option>
+              <option value="health">Health</option>
+              <option value="business">Business</option>
+            </select>
+          </div>
+          <div className="mb-4">
+            <label className="block text-white mb-2">Tags</label>
+            <div className="flex flex-wrap">
+              {tags.map((tag, index) => (
+                <div
+                  key={index}
+                  className="flex items-center mr-2 mb-2 bg-blue-600 rounded px-2 py-1 text-sm"
+                >
+                  {tag}
+                  <button
+                    type="button"
+                    className="ml-2 text-white hover:text-red-400"
+                    onClick={() => handleRemoveTag(index)}
+                  >
+                    &times;
+                  </button>
+                </div>
+              ))}
+              <input
+                type="text"
+                value={newTag}
+                onChange={(e) => setNewTag(e.target.value)}
+                onKeyDown={handleTagKeyDown}
+                placeholder="Press Enter to add a tag"
+                className="w-full px-4 py-2 rounded bg-gray-700 text-white"
+              />
+            </div>
+          </div>
+        </form>
       </div>
-    </Suspense> // Closing Suspense component here
+      <div className="w-1/4 p-6 bg-gray-800">
+        <h2 className="text-xl font-bold mb-4">Options</h2>
+        <div className="mb-4">
+          <label className="block text-white mb-2">Thumbnail</label>
+          {blogId && (
+            <img
+              src={thumbnail}
+              alt="Thumbnail preview"
+              className="w-full h-40 object-cover rounded mb-2"
+            />
+          )}
+          <input
+            className="mb-4"
+            type="file"
+            accept="image/*"
+            onChange={handleFileChange}
+          />
+        </div>
+        <h2 className="text-xl font-bold mb-4">Status</h2>
+        <button
+          onClick={() => toggleStatus("draft")}
+          className={`mb-2 w-full py-2 rounded ${
+            status === "draft" ? "bg-blue-600" : "bg-gray-600"
+          }`}
+        >
+          Draft
+        </button>
+        <button
+          onClick={() => toggleStatus("published")}
+          className={`mb-2 w-full py-2 rounded ${
+            status === "published" ? "bg-blue-600" : "bg-gray-600"
+          }`}
+        >
+          Publish
+        </button>
+        <button
+          onClick={handleThumbnailUpload}
+          disabled={uploading}
+          className="w-full py-2 bg-green-600 rounded hover:bg-green-700"
+        >
+          {blogId ? "Update Blog" : "Add Blog"}
+        </button>
+      </div>
+    </div>
   );
 };
 
