78db272bf00d8b87a105b5d2725d17235a444866
Majid Hasan <majidhasan@Majids-MacBook-Pro.local>
2024-10-13 02:12:43 +0600

    Let's try

--- src/app/api/coins/new-coins/route.js
+++ src/app/api/coins/new-coins/route.js
@@ -2,33 +2,60 @@ import prisma from "@/lib/prisma"; // Ensure you have prisma set up here
 import { NextResponse } from "next/server";
 
 // GET all coins with pagination and search
-export async function GET() {
+// export async function GET() {
+//   try {
+//     const coins = await prisma.coin.findMany({
+//       take: 5, // Fetch 5 coins
+//       orderBy: {
+//         id: "desc", // Latest coins first
+//       },
+//     });
+
+//     // Logging fetched coins for debugging
+//     console.log("Fetched coins:", coins);
+
+//     const response = NextResponse.json({ coins });
+//     // Set headers to prevent caching
+//     response.headers.set(
+//       "Cache-Control",
+//       "no-store, no-cache, must-revalidate, proxy-revalidate"
+//     );
+//     response.headers.set("Pragma", "no-cache");
+//     response.headers.set("Expires", "0");
+
+//     return response;
+//   } catch (error) {
+//     console.error("Error fetching coins:", error);
+//     return NextResponse.json(
+//       { error: "Error fetching coins" },
+//       { status: 500 }
+//     );
+//   }
+// }
+
+export async function GET(request) {
+  const url = new URL(request.url); // Correctly parse the request URL
+  const limit = parseInt(url.searchParams.get("limit")) || 10; // Get the limit parameter and convert to number
+
   try {
+    // Fetch coins from the database based on the condition, including related token contract addresses
     const coins = await prisma.coin.findMany({
-      take: 5, // Fetch 5 coins
+      take: limit,
+      include: {
+        tokenContractAddress: true, // Include related token contract addresses
+      },
       orderBy: {
-        id: "desc", // Latest coins first
+        id: "desc", // Sort coins by total vote count in descending order
       },
     });
 
-    // Logging fetched coins for debugging
-    console.log("Fetched coins:", coins);
-
-    const response = NextResponse.json({ coins });
-    // Set headers to prevent caching
-    response.headers.set(
-      "Cache-Control",
-      "no-store, no-cache, must-revalidate, proxy-revalidate"
-    );
-    response.headers.set("Pragma", "no-cache");
-    response.headers.set("Expires", "0");
-
-    return response;
+    return new Response(JSON.stringify({ coins }), {
+      headers: { "Content-Type": "application/json" },
+    });
   } catch (error) {
-    console.error("Error fetching coins:", error);
-    return NextResponse.json(
-      { error: "Error fetching coins" },
-      { status: 500 }
-    );
+    return new Response(JSON.stringify({ error: "Error fetching coins" }), {
+      status: 500,
+      headers: { "Content-Type": "application/json" },
+    });
   }
 }
